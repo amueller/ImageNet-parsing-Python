01549b98eb2a0ee07cdf3cd05b6e184f7727404e
Andy <amueller@ais.uni-bonn.de>
2011-10-24 20:39:21 +0200

    looking for images with glob...

--- imagenet_analysis.py
+++ imagenet_analysis.py
@@ -1,6 +1,7 @@
 from scipy.io import loadmat
 import numpy as np
 import os
+from glob import glob
 
 import xml.etree.ElementTree as ET
 #import elementtree.ElementTree as ET
@@ -21,9 +22,9 @@ class ImageNetData(object):
         self.children = [np.squeeze(x[5]).astype(np.int) for x in self.synsets]
 
     def get_class(self, search_string):
-        indices = np.where([search_string+', ' in x[2][0] for x in self.synsets])[0]
-        #for i in indices:
-            #print(self.synsets[i])
+        indices = np.where([search_string in x[2][0] for x in self.synsets])[0]
+        for i in indices:
+            print(self.synsets[i])
         return indices
 
     def get_children(self, aclass):
@@ -46,16 +47,36 @@ class ImageNetData(object):
         annotation_file = os.path.join(self.annotation_path, str(wnid), str(wnid) + "_" + str(imageid) + ".xml")
         xmltree = ET.parse(annotation_file)
         bndbox = xmltree.find("object").find("bndbox")
-        [xmin, ymin, xmax, ymax] = [it.text for it in bndbox]
+        #[xmin, ymin, xmax, ymax] = [it.text for it in bndbox]
+        return [it.text for it in bndbox]
+
+    def get_image_files(self, theclass):
+        wnid = self.wnids[theclass]
+        files = glob(os.path.join(self.image_path,wnid,wnid+"*"))
+        print(files)
+
+
+    def bounding_box_images(self, theclass):
+        if not os.path.exists("bounding_box"):
+            os.mkdir("bounding_box")
+        class_string = self.word[theclass]
+        wnid = self.wnids[theclass]
+        if not os.path.exists(wnid):
+            os.mkdir(wnid)
+
+        self.get_image_files(theclass)
 
 
 
 def main():
     imnet = ImageNetData("ILSVRC2011_devkit-2.0/data/meta.mat", "unpacked", "annotation")
-    classes = imnet.get_class("person")
+    classes = imnet.get_class("hammerhead")
     aclass = classes[0]
-    #rchildren = imnet.get_children(aclass)
-    imnet.get_annotation(979, 110)
+    rchildren = imnet.get_children(aclass)
+    imnet.bounding_box_images(aclass)
+    for theclass in rchildren:
+        imnet.bounding_box_images(theclass)
+    #imnet.get_bndbox(979, 110)
     tracer()
 
 if __name__ == "__main__":
