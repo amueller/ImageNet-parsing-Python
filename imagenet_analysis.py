d6297922aac8c3471358fdf88505a52c3db70aa9
Andy <amueller@ais.uni-bonn.de>
2011-10-24 18:44:40 +0200

    xml parsing for bounding box annotation.

--- imagenet_analysis.py
+++ imagenet_analysis.py
@@ -1,15 +1,21 @@
 from scipy.io import loadmat
 import numpy as np
+import os
+
+import xml.etree.ElementTree as ET
+#import elementtree.ElementTree as ET
+
 from IPython.core.debugger import Tracer
 tracer = Tracer()
 
 class ImageNetData(object):
-    def __init__(self, meta_path, image_path):
+    def __init__(self, meta_path, image_path, annotation_path):
         self.image_path = image_path
+        self.annotation_path = annotation_path
         self.meta_data = loadmat(meta_path)
         self.synsets = np.squeeze(self.meta_data['synsets'])
         self.ids = np.squeeze(np.array([x[0] for x in self.synsets]))
-        self.wids = np.squeeze(np.array([x[1] for x in self.synsets]))
+        self.wnids = np.squeeze(np.array([x[1] for x in self.synsets]))
         self.word = np.squeeze(np.array([x[2] for x in self.synsets]))
         self.num_children = np.squeeze(np.array([x[4] for x in self.synsets]))
         self.children = [np.squeeze(x[5]).astype(np.int) for x in self.synsets]
@@ -25,7 +31,7 @@ class ImageNetData(object):
         children = self.synsets[aclass][5][0] - 1
 
         print(self.synsets[aclass])
-        rchildren = []
+        rchildren = children.tolist()
 
         print "-----------------"
         for child in children:
@@ -33,14 +39,23 @@ class ImageNetData(object):
             # recurse
             if self.synsets[child][4] != 0:
                 rchildren.extend(self.get_children(child))
-        return children, rchildren
+        return rchildren
+
+    def get_bndbox(self, classid, imageid):
+        wnid = self.wnids[classid]
+        annotation_file = os.path.join(self.annotation_path, str(wnid), str(wnid) + "_" + str(imageid) + ".xml")
+        xmltree = ET.parse(annotation_file)
+        bndbox = xmltree.find("object").find("bndbox")
+        [xmin, ymin, xmax, ymax] = [it.text for it in bndbox]
+
 
 
 def main():
-    imnet = ImageNetData("ILSVRC2011_devkit-2.0/data/meta.mat","unpacked")
+    imnet = ImageNetData("ILSVRC2011_devkit-2.0/data/meta.mat", "unpacked", "annotation")
     classes = imnet.get_class("person")
     aclass = classes[0]
-    children, rchildren = imnet.get_children(aclass)
+    #rchildren = imnet.get_children(aclass)
+    imnet.get_annotation(979, 110)
     tracer()
 
 if __name__ == "__main__":
